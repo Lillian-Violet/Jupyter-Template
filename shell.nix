b871277628324c8ef5bc06f2c2dd40da619db1a7
Lillian Violet <git@gladtherescake.eu>
2023-10-11 22:53:13 +0200

    Clarify how to start nix shell

--- shell.nix
+++ shell.nix
@@ -1,3 +1,4 @@
+# Nis shell configuration, use nix-shell to start a shell in the terminal
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
