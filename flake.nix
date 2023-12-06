3b3b2e5bdda3603a8c56faa66c6419dc8c574885
Lillian-Violet <git@gladtherescake.eu>
2023-12-06 15:03:54 +0100

    Add ipykernel

--- flake.nix
+++ flake.nix
@@ -7,23 +7,30 @@
       url = "github:numtide/flake-utils";
     };
   };
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in rec {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          (python3.withPackages(ps: with ps; [
-            ipython
-            jupyter
-            numpy
-            pandas
-          ]))
-        ];
-        shellHook = "jupyter notebook";
-      };
-    }
-  );
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python3.withPackages (ps:
+              with ps; [
+                ipython
+                ipykernel
+                jupyter
+                numpy
+                pandas
+              ]))
+          ];
+          shellHook = "jupyter notebook";
+        };
+      }
+    );
 }
