35cd0483ff6b228c9f8e403ea0d064959d65066c
Lillian-Violet <git@gladtherescake.eu>
2023-12-06 15:01:31 +0100

    Add flake lock and set to flake

--- shell.nix
+++ shell.nix
@@ -1,13 +1,13 @@
-# Nis shell configuration, use nix-shell to start a shell in the terminal
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ 
-      python310
-      python310Packages.notebook
-      python310Packages.pandas
-      python310Packages.geopandas
-      python310Packages.numpy
-      python310Packages.matplotlib
-      ];
+# Nix shell configuration, use nix-shell to start a shell in the terminal
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs.buildPackages; [
+    python310
+    python310Packages.notebook
+    python310Packages.pandas
+    python310Packages.geopandas
+    python310Packages.numpy
+    python310Packages.matplotlib
+  ];
 }
